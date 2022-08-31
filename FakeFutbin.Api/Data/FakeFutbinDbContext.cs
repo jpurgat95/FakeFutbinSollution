139c42e0a8566c0410f4bc51c6057888054d0557
jpurgat95 <j.purgat95@gmail.com>
2022-08-31 18:35:10 +0200

    Api: DbContetext class created

---
+++ FakeFutbin.Api/Data/FakeFutbinDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FakeFutbin.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeFutbin.Api.Data;
+
+public class FakeFutbinDbContext: DbContext
+{
+    public FakeFutbinDbContext(DbContextOptions<FakeFutbinDbContext> options) : base(options)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<Scout> Scouts { get; set; }
+    public DbSet<ScoutPlayer> ScoutPlayers { get; set; }
+    public DbSet<Player> Players { get; set; }
+    public DbSet<PlayerNationality> PlayerNationalities { get; set; }
+    public DbSet<Coach> Coaches { get; set; }
+}
