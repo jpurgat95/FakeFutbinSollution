ba3e0f78a9a7a172b185d5b826439fc57af6846c
jpurgat95 <j.purgat95@gmail.com>
2022-08-31 18:37:19 +0200

    Api: register FakeFutbinDbContext for Di/ adding connection string

--- FakeFutbin.Api/Program.cs
+++ FakeFutbin.Api/Program.cs
@@ -1,3 +1,6 @@
+using FakeFutbin.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DI for FakeFutbinDbContext
+builder.Services.AddDbContextPool<FakeFutbinDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("FakeFutbinConnection"))
+);
+
 
 var app = builder.Build();
 
