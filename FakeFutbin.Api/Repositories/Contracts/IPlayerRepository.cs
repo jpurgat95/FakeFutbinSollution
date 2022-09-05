ef0cf1bb83f7ff149eab6c8171d274aca9c439a9
jpurgat95 <j.purgat95@gmail.com>
2022-09-05 16:59:57 +0200

    Player Repository class created, two methods logic defined: GetPlayers and GetNationalities

---
+++ FakeFutbin.Api/Repositories/Contracts/IPlayerRepository.cs
@@ -0,0 +1,14 @@
+﻿using FakeFutbin.Api.Entities;
+
+namespace FakeFutbin.Api.Repositories.Contracts;
+
+public interface IPlayerRepository
+{
+    Task<IEnumerable<Player>> GetPlayers();
+    Task<IEnumerable<PlayerNationality>> GetNationalities();
+    Task<Player> GetPlayer(int id);
+    Task<PlayerNationality> GetNationality(int id);
+    Task<IEnumerable<Player>> GetPlayersByNationality(int id);
+
+
+}
---
+++ FakeFutbin.Api/Repositories/PlayerRepository.cs
@@ -0,0 +1,42 @@
+﻿using FakeFutbin.Api.Data;
+using FakeFutbin.Api.Entities;
+using FakeFutbin.Api.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeFutbin.Api.Repositories;
+
+public class PlayerRepository : IPlayerRepository
+{
+    private readonly FakeFutbinDbContext _fakeFutbinDbContext;
+
+    public PlayerRepository(FakeFutbinDbContext fakeFutbinDbContext)
+    {
+        _fakeFutbinDbContext = fakeFutbinDbContext;
+    }
+    public async Task<IEnumerable<PlayerNationality>> GetNationalities()
+    {
+        var nationalities = await _fakeFutbinDbContext.PlayerNationalities.ToListAsync();
+        return nationalities;
+    }
+
+    public Task<PlayerNationality> GetNationality(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Player> GetPlayer(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Player>> GetPlayers()
+    {
+        var players = await _fakeFutbinDbContext.Players.ToListAsync();
+        return players;
+    }
+
+    public Task<IEnumerable<Player>> GetPlayersByNationality(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
