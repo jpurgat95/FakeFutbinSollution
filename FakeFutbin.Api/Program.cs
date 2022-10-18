e15f2475976ec531285349b17b2224d80d2f6706
jpurgat95 <j.purgat95@gmail.com>
2022-10-18 17:25:24 +0200

    Register and Login functionalities added to API

--- FakeFutbin.Api/Program.cs
+++ FakeFutbin.Api/Program.cs
@@ -1,8 +1,15 @@
-using FakeFutbin.Api.Data;
-using FakeFutbin.Api.Repositories;
-using FakeFutbin.Api.Repositories.Contracts;
-using Microsoft.EntityFrameworkCore;
+global using FakeFutbin.Api.Entities;
+global using FakeFutbin.Models.Dto;
+global using FakeFutbin.Api.Repositories.Contracts;
+global using FakeFutbin.Api.Data;
+global using FakeFutbin.Api.Repositories;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Authorization;
 using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,12 +18,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme (\"Bearer {token} \")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+//DI for AuthRepos
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
 //DI for FakeFutbinDbContext, PlayerRepository, ScoutRepository
 builder.Services.AddDbContextPool<FakeFutbinDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("FakeFutbinConnection"))
 );
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
 builder.Services.AddScoped<ICoachRepository, CoachRepository>();
 
@@ -38,6 +73,8 @@ policy.WithOrigins("https://localhost:7295", "http://localhost:7295")
 );
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ FakeFutbin.Api/Repositories/Contracts/IAuthRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace FakeFutbin.Api.Repositories.Contracts;
+
+public interface IAuthRepository
+{
+    Task<User> RegisterUser(UserDto request);
+    Task<AuthResponseDto> Login(UserDto request);
+    Task<AuthResponseDto> RefreshToken();
+}
