793fcc28113e5d75568fb9f37c6a09e3bfd100ff
jpurgat95 <j.purgat95@gmail.com>
2022-10-18 17:00:04 +0200

    UserDto and AuthResponseDto created

---
+++ FakeFutbin.Models/Dto/AuthResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeFutbin.Models.Dto;
+
+public class AuthResponseDto
+{
+    public bool Success { get; set; } = false;
+    public string Message { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+    public DateTime TokenExpires { get; set; }
+}
---
+++ FakeFutbin.Models/Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeFutbin.Models.Dto;
+
+public class UserDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
